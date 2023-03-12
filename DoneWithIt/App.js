9a96292cf663779f31df65050213e66fd6c33bc2
Rab-son <rabsonsayendajnr@gmail.com>
2023-03-12 08:36:07 +0200

    first app

---
+++ DoneWithIt/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, Image, SafeAreaView } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View
+      style={{
+        backgroundColor: "dodgerblue",
+        flex: 1
+      }}>
+
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          flex: 1
+        }} />
+
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 1
+        }} />
+
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1
+        }} />
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+});
