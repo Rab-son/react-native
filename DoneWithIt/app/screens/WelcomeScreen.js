54fe1d2628830452e30dacf41b059c22d65695f0
Rab-son <rabsonsayendajnr@gmail.com>
2023-03-12 10:15:51 +0200

    improving the welcome screen

--- DoneWithIt/app/screens/WelcomeScreen.js
+++ DoneWithIt/app/screens/WelcomeScreen.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import AppButton from '../components/AppButton';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground
+            blurRadius={10}
             style={styles.background}
             source={require('../assets/background.jpg')}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-                <Text> Sell What You Don't Need</Text>
+                <Text style={styles.tagline}> Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" />
+                <AppButton title="Register" color="secondary" />
+            </View>
         </ImageBackground>
     );
 }
 
 
 const styles = StyleSheet.create({
+
     background: {
         flex: 1,
         justifyContent: "flex-end",
         alignItems: "center"
     },
 
-    loginButton: {
+    buttonsContainer: {
+        padding: 20,
         width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",
     },
+
 
     logo: {
         width: 100,
@@ -42,11 +47,13 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
 
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#4ecdc4",
-    },
+    tagline: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
+    }
+
+
 
 })
 
