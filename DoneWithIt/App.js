aa6fe7b64f8f65ae9e72f1be8d464197ede58582
Rab-son <rabsonsayendajnr@gmail.com>
2023-03-13 07:40:36 +0200

    icon component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -3,11 +3,20 @@ import AppButton from './app/components/AppButton';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
+
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <Screen>
+    <Icon
+      name="email"
+      size={100}
+
+    />
+  </Screen>;
 }
 
 
